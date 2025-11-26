af119b47e1a3862c1550aab4683b22b2c5f74a17
Çağrı Taşkın <98661878+cagritaskn@users.noreply.github.com>
2025-11-26 17:07:16 +0300

    New build script for .NET 8.0

--- src/build_simple.bat
+++ src/build_simple.bat
@@ -48,24 +48,25 @@ if errorlevel 1 (
 )
 
 REM Create res folder in output directory
-if not exist "bin\Release\net6.0-windows\res" mkdir "bin\Release\net6.0-windows\res"
+if not exist "bin\Release\net8.0-windows\res" mkdir "bin\Release\net8.0-windows\res"
 
 REM Copy resource files to res folder
-if exist "Resources\splitwire.ico" copy "Resources\splitwire.ico" "bin\Release\net6.0-windows\res\"
-if exist "Resources\splitwire-logo-128.png" copy "Resources\splitwire-logo-128.png" "bin\Release\net6.0-windows\res\"
-if exist "Resources\splitwireturkeytext.png" copy "Resources\splitwireturkeytext.png" "bin\Release\net6.0-windows\res\"
-if exist "Resources\wiresock-vpn-client-x64-1.4.7.1.msi" copy "Resources\wiresock-vpn-client-x64-1.4.7.1.msi" "bin\Release\net6.0-windows\res\"
+if exist "Resources\splitwire.ico" copy "Resources\splitwire.ico" "bin\Release\net8.0-windows\res\"
+if exist "Resources\splitwire-logo-128.png" copy "Resources\splitwire-logo-128.png" "bin\Release\net8.0-windows\res\"
+if exist "Resources\splitwireturkeytext.png" copy "Resources\splitwireturkeytext.png" "bin\Release\net8.0-windows\res\"
+if exist "Resources\wiresock-vpn-client-x64-1.4.7.1.msi" copy "Resources\wiresock-vpn-client-x64-1.4.7.1.msi" "bin\Release\net8.0-windows\res\"
 
 REM Copy language files to res folder
 if exist "Resources\Languages" (
-    if not exist "bin\Release\net6.0-windows\res\Languages" mkdir "bin\Release\net6.0-windows\res\Languages"
-    if exist "Resources\Languages\tr.json" copy "Resources\Languages\tr.json" "bin\Release\net6.0-windows\res\Languages\"
-    if exist "Resources\Languages\en.json" copy "Resources\Languages\en.json" "bin\Release\net6.0-windows\res\Languages\"
-    if exist "Resources\Languages\ru.json" copy "Resources\Languages\ru.json" "bin\Release\net6.0-windows\res\Languages\"
+    if not exist "bin\Release\net8.0-windows\res\Languages" mkdir "bin\Release\net8.0-windows\res\Languages"
+    if exist "Resources\Languages\tr.json" copy "Resources\Languages\tr.json" "bin\Release\net8.0-windows\res\Languages\"
+    if exist "Resources\Languages\en.json" copy "Resources\Languages\en.json" "bin\Release\net8.0-windows\res\Languages\"
+    if exist "Resources\Languages\ru.json" copy "Resources\Languages\ru.json" "bin\Release\net8.0-windows\res\Languages\"
+    if exist "Resources\Languages\es.json" copy "Resources\Languages\es.json" "bin\Release\net8.0-windows\res\Languages\"
 )
 
 REM Check if critical files exist
-if not exist "bin\Release\net6.0-windows\SplitWire-Turkey.exe" (
+if not exist "bin\Release\net8.0-windows\SplitWire-Turkey.exe" (
     echo Error: Main executable not found! Build may have failed.
     cd ..
     pause
@@ -73,16 +74,16 @@ if not exist "bin\Release\net6.0-windows\SplitWire-Turkey.exe" (
 )
 
 REM Clean up unnecessary files (keep only .exe and .dll)
-if exist "bin\Release\net6.0-windows\SplitWire-Turkey.deps.json" del "bin\Release\net6.0-windows\SplitWire-Turkey.deps.json"
-if exist "bin\Release\net6.0-windows\SplitWire-Turkey.pdb" del "bin\Release\net6.0-windows\SplitWire-Turkey.pdb"
-if exist "bin\Release\net6.0-windows\SplitWire-Turkey.xml" del "bin\Release\net6.0-windows\SplitWire-Turkey.xml"
+if exist "bin\Release\net8.0-windows\SplitWire-Turkey.deps.json" del "bin\Release\net8.0-windows\SplitWire-Turkey.deps.json"
+if exist "bin\Release\net8.0-windows\SplitWire-Turkey.pdb" del "bin\Release\net8.0-windows\SplitWire-Turkey.pdb"
+if exist "bin\Release\net8.0-windows\SplitWire-Turkey.xml" del "bin\Release\net8.0-windows\SplitWire-Turkey.xml"
 
 REM Copy AddToRoot contents to Release folder
 echo.
 echo AddToRoot klasoru icerigi Release klasorune kopyalaniyor...
 cd ..
 if exist "AddToRoot" (
-    xcopy "AddToRoot\*" "SplitWireTurkey\bin\Release\net6.0-windows\" /E /I /Y
+    xcopy "AddToRoot\*" "SplitWireTurkey\bin\Release\net8.0-windows\" /E /I /Y
     if errorlevel 1 (
         echo Error: AddToRoot icerigi kopyalanirken hata olustu!
         pause
@@ -98,9 +99,9 @@ if exist "AddToRoot" (
 
 echo.
 echo Build complete!
-echo Executable created in: SplitWireTurkey\bin\Release\net6.0-windows\SplitWire-Turkey.exe
-echo Resource files copied to: SplitWireTurkey\bin\Release\net6.0-windows\res\
-echo AddToRoot contents copied to: SplitWireTurkey\bin\Release\net6.0-windows\
+echo Executable created in: SplitWireTurkey\bin\Release\net8.0-windows\SplitWire-Turkey.exe
+echo Resource files copied to: SplitWireTurkey\bin\Release\net8.0-windows\res\
+echo AddToRoot contents copied to: SplitWireTurkey\bin\Release\net8.0-windows\
 echo.
 echo Note: This is a simple build without publishing. Run the executable from the bin folder.
 echo.
